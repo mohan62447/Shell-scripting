ffc3da836677dd85f177c07d31816ab8358b90d5
mohan62447 <reddymohan1997@gmail.com>
2026-06-16 19:40:14 +0530

    added arrays

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -4,24 +4,29 @@ LOG_DIR="/c/Users/Asus/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs"
 APP_LOG_FILE="application.log"
 SYS_LOG_FILE="system.log"
 
+ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
+
 echo "Analyzing logs in the current directory..."
 echo "==============================="
 find $LOG_DIR -name "*.log" -mtime -1
 
 echo -e "\nApplication Log Analysis:"
-grep "ERROR" "$LOG_DIR/$APP_LOG_FILE"
+grep "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of Errors logs in application.log:"
-grep -c "ERROR" "$LOG_DIR/$APP_LOG_FILE"
+grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of Fatal Errors in application.log:"
-grep -c "FATAL" "$LOG_DIR/$APP_LOG_FILE"
+grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of Errors logs in system.log:"
-grep -c "ERROR" "$LOG_DIR/$SYS_LOG_FILE"
+grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$SYS_LOG_FILE"
 
 echo -e "\nNumber of Fatal Errors in system.log:"
-grep -c "FATAL" "$LOG_DIR/$SYS_LOG_FILE"
+grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$SYS_LOG_FILE"
+
+echo -e "\nNumber of Critical Errors in system.log:"
+grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
 
 echo -e "\nSystem Log Analysis:"
-grep "ERROR" "$LOG_DIR/$SYS_LOG_FILE"
+grep "${ERROR_PATTERNS[0]}" "$LOG_DIR/$SYS_LOG_FILE"
