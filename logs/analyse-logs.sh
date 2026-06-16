8a02d557c375a788ee48b83be2ad6174c227df80
mohan62447 <reddymohan1997@gmail.com>
2026-06-16 18:56:46 +0530

    added seperation line

--- logs/analyse-logs.sh
+++ logs/analyse-logs.sh
@@ -3,20 +3,20 @@ echo "Analyzing logs in the current directory..."
 echo "==============================="
 find . -name "*.log" -mtime -1
 
-echo "Application Log Analysis:"
+echo -e "\nApplication Log Analysis:"
 grep "ERROR" application.log
 
-echo "Number of Errors logs in application.log:"
+echo -e "\nNumber of Errors logs in application.log:"
 grep -c "ERROR" application.log
 
-echo "Number of Fatal Errors in application.log:"
+echo -e "\nNumber of Fatal Errors in application.log:"
 grep -c "FATAL" application.log
 
-echo "Number of Errors logs in system.log:"
+echo -e "\nNumber of Errors logs in system.log:"
 grep -c "ERROR" system.log
 
-echo "Number of Fatal Errors in system.log:"
+echo -e "\nNumber of Fatal Errors in system.log:"
 grep -c "FATAL" system.log
 
-echo "System Log Analysis:"
+echo -e "\nSystem Log Analysis:"
 grep "ERROR" system.log
