f5908715d98b7faf92874d38241b0d37db45f2e3
mohan62447 <reddymohan1997@gmail.com>
2026-06-16 19:08:37 +0530

    added absulate path

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 echo "Analyzing logs in the current directory..."
 echo "==============================="
-find . -name "*.log" -mtime -1
+find ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs -name "*.log" -mtime -1
 
 echo -e "\nApplication Log Analysis:"
-grep "ERROR" application.log
+grep "ERROR" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/application.log
 
 echo -e "\nNumber of Errors logs in application.log:"
-grep -c "ERROR" application.log
+grep -c "ERROR" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/application.log
 
 echo -e "\nNumber of Fatal Errors in application.log:"
-grep -c "FATAL" application.log
+grep -c "FATAL" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/application.log
 
 echo -e "\nNumber of Errors logs in system.log:"
-grep -c "ERROR" system.log
+grep -c "ERROR" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/system.log
 
 echo -e "\nNumber of Fatal Errors in system.log:"
-grep -c "FATAL" system.log
+grep -c "FATAL" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/system.log
 
 echo -e "\nSystem Log Analysis:"
-grep "ERROR" system.log
+grep "ERROR" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/system.log
