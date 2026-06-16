343b16e5768054b6127cc2c751b669a66a34d034
mohan62447 <reddymohan1997@gmail.com>
2026-06-16 18:50:37 +0530

    added echo to summarize

--- logs/analyse-logs.sh
+++ logs/analyse-logs.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+echo "Analyzing logs in the current directory..."
+echo "==============================="
 find . -name "*.log" -mtime -1
 
+echo "Application Log Analysis:"
 grep "ERROR" application.log
+
+echo "Number of Errors logs in application.log:"
 grep -c "ERROR" application.log
+
+echo "Number of Fatal Errors in application.log:"
 grep -c "FATAL" application.log
 
+echo "Number of Errors logs in system.log:"
 grep -c "ERROR" system.log
+
+echo "Number of Fatal Errors in system.log:"
 grep -c "FATAL" system.log
+
+echo "System Log Analysis:"
 grep "ERROR" system.log
