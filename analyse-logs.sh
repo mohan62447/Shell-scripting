0d0c18e13e772afab9aceaab17b3cbcb6f901367
mohan62447 <reddymohan1997@gmail.com>
2026-06-16 19:14:48 +0530

    Added variables

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+
+LOG_DIR="~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs"
+APP_LOG_FILE="application.log"
+SYS_LOG_FILE="system.log"
+
 echo "Analyzing logs in the current directory..."
 echo "==============================="
-find ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs -name "*.log" -mtime -1
+find $LOG_DIR -name "*.log" -mtime -1
 
 echo -e "\nApplication Log Analysis:"
-grep "ERROR" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/application.log
+grep "ERROR" $LOG_DIR/$APP_LOG_FILE
 
 echo -e "\nNumber of Errors logs in application.log:"
-grep -c "ERROR" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/application.log
+grep -c "ERROR" $LOG_DIR/$APP_LOG_FILE
 
 echo -e "\nNumber of Fatal Errors in application.log:"
-grep -c "FATAL" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/application.log
+grep -c "FATAL" $LOG_DIR/$APP_LOG_FILE
 
 echo -e "\nNumber of Errors logs in system.log:"
-grep -c "ERROR" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/system.log
+grep -c "ERROR" $LOG_DIR/$SYS_LOG_FILE
 
 echo -e "\nNumber of Fatal Errors in system.log:"
-grep -c "FATAL" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/system.log
+grep -c "FATAL" $LOG_DIR/$SYS_LOG_FILE
 
 echo -e "\nSystem Log Analysis:"
-grep "ERROR" ~/OneDrive/desktop/DevOps_Lab/Shell-scripting/logs/system.log
+grep "ERROR" $LOG_DIR/$SYS_LOG_FILE
