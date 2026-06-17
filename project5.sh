cd673d47545fade87f9dccb6a7a6ce85468abfc0
mohan62447 <reddymohan1997@gmail.com>
2026-06-17 22:28:11 +0530

    project5 commit

---
+++ project5.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+echo "Downloading the promethueus binary file from the official website"
+if [ -e /home/ubuntu/prometheus-3.6.0.linux-amd64.tar.gz ]; then
+    echo "Prometheus binary file already exists. Skipping download."
+    tar -zxvf prometheus-3.6.0.linux-amd64.tar.gz
+else
+echo "Prometheus binary file does not exist. Downloading..."
+    wget https://github.com/prometheus/prometheus/releases/download/v3.6.0/prometheus-3.6.0.linux-amd64.tar.gz
+    tar -zxvf prometheus-3.6.0.linux-amd64.tar.gz
+    echo "Prometheus binary file downloaded and extracted successfully."
+    fi
