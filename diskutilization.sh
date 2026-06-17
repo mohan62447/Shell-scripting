a8b4d01822b9585160d4d0d0f5004b9cb40d5c6f
mohan62447 <reddymohan1997@gmail.com>
2026-06-17 15:31:32 +0530

    commit2

--- diskutilization.sh
+++ diskutilization.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "check disk usage in my system"
-disk_size=$(df -h | grep "C/Program Files/Git" | awk 'NR==2 {gsub("%","",$6); print $6}')
+disk_size=$(df -h | grep "C/Program Files/Git" | awk 'NR==1 {gsub("%","",$6); print $6}')
 echo "$disk_size% disk usage is above the threshold of 80%"
 if [ "$disk_size" -gt 80 ]; then
     echo "Disk usage is above the threshold. Please take necessary actions."
