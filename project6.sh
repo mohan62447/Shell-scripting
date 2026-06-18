bfaeeddd08b79ea3e8e802bc1c9b8aa08be02ce1
mohan62447 <reddymohan1997@gmail.com>
2026-06-18 12:07:55 +0530

    done

--- project6.sh
+++ project6.sh
@@ -3,9 +3,9 @@
 # How to use a for loop in shell scripting to loop over entries?
 
 # Project: We will loop over the contents of a specified directory and delete all the contents one by one.
-for folder in $(find /home/ubuntu/test -type d); do
+for folder in $(find /home/ubuntu/ -type d); do
 echo "the folder is $folder"
-if [ -d "/home/ubuntu/test" ]; then
+if [ -d "test" ]; then
     echo "Deleting the folder $folder"
     rm -rf "/home/ubuntu/test"
     echo "Folder $folder deleted successfully."
