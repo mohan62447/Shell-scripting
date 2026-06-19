5efcbc9897e3a576c5023cc9f3d31ca02e52a465
mohan62447 <reddymohan1997@gmail.com>
2026-06-19 11:48:52 +0530

    done

---
+++ project8.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: Please provide software name as an argument."
+    exit 1
+fi
+
+if [ $(id -u) -ne 0 ]; then
+    echo "This script must be run as root. Please use sudo."
+    exit 2
+fi
+
+for software in "$@"; do
+    if which $software &>/dev/null  then
+        echo "$software is already installed."
+    else
+        echo "$software is not installed. Installing..."
+        apt update && apt install -y $software
+        if [ $? -eq 0 ]; then
+            echo "$software installed successfully."
+        else
+            echo "Failed to install $software. Please check your internet connection and package name."
+        fi
+    fi
+    done
