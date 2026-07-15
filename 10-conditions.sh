7eb17513613971e8485848cc7a6646bf5ba4d424
mohan62447 <reddymohan1997@gmail.com>
2026-07-15 20:28:38 +0530

    new commit

---
+++ 10-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 10 ]
+then
+    echo "The number is positive"
+elif [ $NUMBER -lt 10 ]
+then
+    echo "The number is negative"
+else
+    echo "The number is ten"
+fi
