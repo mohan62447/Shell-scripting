edf3d1aed26b81a89b943b431f6f3a46621303e9
mohan62447 <reddymohan1997@gmail.com>
2026-07-15 16:43:51 +0530

    new commit

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=Modi
+person2=Musk
+
+echo "person1:: Hi person2, How are you?"
+echo "person2:: Hello person1, I am good, what about you?"
+echo "person1:: I am fine too..what's up"
+echo "person2:: I am going to mars will you come"
+echo "person1:: I will come once you come back"
