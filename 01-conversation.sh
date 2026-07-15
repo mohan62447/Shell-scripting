1e2ba3d96a396caf361577d7e0a2a02094ca0cfc
mohan62447 <reddymohan1997@gmail.com>
2026-07-15 13:57:39 +0530

    new commit

---
+++ 01-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Modi:: Hi Musk, How are you?"
+echo "Musk:: Hello Modi, I am good, what about you?"
+echo "Modi:: I am fine too..what's up"
+echo "Musk:: I am going to mars will you come"
+echo "Modi:: I will come once you come back"
